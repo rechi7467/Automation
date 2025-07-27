ba6a5f158eb48e945895a751b8b9e483db692a43
rechi7467 <6737467r@gmail.com>
2025-07-27 19:31:14 +0300

    Update StartTimer.java

--- src/main/java/pages/StartTimer.java
+++ src/main/java/pages/StartTimer.java
@@ -1,47 +1,22 @@
-// package pages;
-
-// import io.appium.java_client.android.AndroidDriver;
-// import io.appium.java_client.pagefactory.AndroidFindBy;
-// import org.openqa.selenium.WebElement;
-
-// public class StartTimer extends BasePage{
-//     @AndroidFindBy(id = "com.google.android.deskclock:id/timer_setup_digit_8")
-//     private WebElement number8;
-
-//     @AndroidFindBy(id = "com.google.android.deskclock:id/timer_setup_time")
-//     private WebElement end;
-//     public StartTimer(AndroidDriver driver){
-//         super(driver);
-//     }
-//     public void findTime(){
-//         elementToBeClickable(number8).click();
-
-//         if( visibilityOf(end).getText().contains(number8.getText())
-//     }
-// }
 package pages;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import org.openqa.selenium.WebElement;
 
-public class StartTimer extends BasePage {
+public class StartTimer extends BasePage{
     @AndroidFindBy(id = "com.google.android.deskclock:id/timer_setup_digit_8")
     private WebElement number8;
 
     @AndroidFindBy(id = "com.google.android.deskclock:id/timer_setup_time")
     private WebElement end;
-
-    public StartTimer(AndroidDriver driver) {
+    public StartTimer(AndroidDriver driver){
         super(driver);
     }
-
-    public void findTime() {
+    public void findTime(){
         elementToBeClickable(number8).click();
 
-        if (visibilityOf(end).getText().contains(number8.getText())) {
-            // כאן תוכל להוסיף את הקוד שצריך שירוץ אם התנאי מתקיים
-            System.out.println("Timer setup contains number 8");
-        }
+        if( visibilityOf(end).getText().contains(number8.getText())
     }
 }
+
